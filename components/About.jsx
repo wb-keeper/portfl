2f2daf0652f2eb2912af09047f336d4097793641
wb-keeper <wb-keeper@mail.ru>
2023-12-28 19:50:04 +0300

    Working on Skill tab

--- components/About.jsx
+++ components/About.jsx
@@ -182,11 +182,11 @@ function About() {
                 </TabsContent>
                 <TabsContent value="qualifications">
                   <div>
-                    <h3 className="h3 text-center xl:text-left">
+                    <h3 className="h3 text-center xl:text-left mb-8">
                       My Awesome Journey
                     </h3>
-                    <div>
-                      <div>
+                    <div className="grid md:grid-cols-2 gap-y-8">
+                      <div className="flex flex-col gap-y-6">
                         <div className="flex gap-x-4 items-center text-[22px] text-primary">
                           <Briefcase />
 
@@ -199,10 +199,7 @@ function About() {
                             (item, index) => {
                               const { company, role, years } = item;
                               return (
-                                <div
-                                  className="flex gap-x-8 group bg-yellow-200"
-                                  key={index}
-                                >
+                                <div className="flex gap-x-8 group" key={index}>
                                   <div className="h-[84px] w-[1px] bg-border relative ml-2">
                                     <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
                                   </div>
@@ -224,11 +221,53 @@ function About() {
                         </div>
                       </div>
 
-                      <div>education</div>
+                      <div className="flex flex-col gap-y-6">
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                          <GraduationCap size={28} />
+
+                          <h4 className="capitalize font-medium">
+                            {getData(qualificationData, "education").title}
+                          </h4>
+                        </div>
+                        <div className="flex flex-col gap-y-8">
+                          {getData(qualificationData, "education").data.map(
+                            (item, index) => {
+                              const { university, qualification, years } = item;
+                              return (
+                                <div className="flex gap-x-8 group" key={index}>
+                                  <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                  </div>
+                                  <div>
+                                    <div className="font-semibold text-xl leading-none mb-2">
+                                      {university}
+                                    </div>
+                                    <div className="text-lg leading-none text-muted-foreground mb-4">
+                                      {qualification}
+                                    </div>
+                                    <div className="text-base font-medium">
+                                      {years}
+                                    </div>
+                                  </div>
+                                </div>
+                              );
+                            },
+                          )}
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </TabsContent>
-                <TabsContent value="skills">skills info</TabsContent>
+                <TabsContent value="skills">
+                  <div className="text-center xl:text-left">
+                    <h3 className="h3 mb-8">Tools I Use Everyday</h3>
+                    <div>
+                      <h4 className="text-xl font-semibold mb-2">Skills</h4>
+                      <div className="border-b border-border mb-4"></div>
+                      <div></div>
+                    </div>
+                  </div>
+                </TabsContent>
               </div>
             </Tabs>
           </div>
