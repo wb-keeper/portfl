ca08cb6a3c6efd917e599bf1e7fd3e41c9a2e741
wb-keeper <wb-keeper@mail.ru>
2023-12-29 08:03:18 +0300

    Finished About components and started Services

--- components/About.jsx
+++ components/About.jsx
@@ -260,11 +260,47 @@ function About() {
                 </TabsContent>
                 <TabsContent value="skills">
                   <div className="text-center xl:text-left">
-                    <h3 className="h3 mb-8">Tools I Use Everyday</h3>
-                    <div>
+                    <h3 className="h3 mb-8">What I Use Everyday</h3>
+                    <div className="mb-16">
                       <h4 className="text-xl font-semibold mb-2">Skills</h4>
                       <div className="border-b border-border mb-4"></div>
-                      <div></div>
+                      <div>
+                        {getData(skillData, "skills").data.map(
+                          (item, index) => {
+                            const { name } = item;
+                            return (
+                              <div
+                                key={index}
+                                className="w-2/4 text-center xl:text-left mx-auto xl:mb-0"
+                              >
+                                <div className="font-medium">{name}</div>
+                              </div>
+                            );
+                          },
+                        )}
+                      </div>
+                    </div>
+                    <div>
+                      <h4 className="text-xl font-semibold mb-2 xl:text-left">
+                        Tools
+                      </h4>
+                      <div className="border-b border-border mb-4"></div>
+                      <div className="flex gap-x-8 justify-center xl:justify-start">
+                        {getData(skillData, "tools").data.map((item, index) => {
+                          const { imgPath } = item;
+                          return (
+                            <div>
+                              <Image
+                                src={imgPath}
+                                height={48}
+                                width={48}
+                                priority
+                                alt="img"
+                              />
+                            </div>
+                          );
+                        })}
+                      </div>
                     </div>
                   </div>
                 </TabsContent>
