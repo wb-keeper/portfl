55f260b631e84fe6170237f555243cd6b656d666
wb-keeper <wb-keeper@mail.ru>
2023-12-31 19:33:19 +0300

    added servicedata

---
+++ components/Services.jsx
@@ -0,0 +1,14 @@
+import { GanttChartSquare } from "lucide-react";
+
+const serviceData = [
+  {
+    icon: <GanttChartSquare size={72} strokeWidth={0.8} />,
+    title: "Web Design",
+    description: "",
+  },
+];
+function Services(props) {
+  return <div>Services</div>;
+}
+
+export default Services;
