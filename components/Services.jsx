95f0cb87cae24113e66318a0e7ec4100f600c91a
wb-keeper <wb-keeper@mail.ru>
2024-01-01 21:22:01 +0300

    Minor changes

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,14 +1,31 @@
-import { GanttChartSquare } from "lucide-react";
+import { Blocks, GanttChartSquare, Gem } from "lucide-react";
 
 const serviceData = [
   {
     icon: <GanttChartSquare size={72} strokeWidth={0.8} />,
     title: "Web Design",
-    description: "",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  },
+  {
+    icon: <Blocks size={72} strokeWidth={0.8} />,
+    title: "Web Development",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    lorem,
+  },
+  {
+    icon: <Gem size={72} strokeWidth={0.8} />,
+    title: "App Development",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
   },
 ];
 function Services(props) {
-  return <div>Services</div>;
+  return (
+    <section className="mb-12 xl:mb-36">
+      <div className="container mx-auto">
+        <h2 className="section-title">My Service</h2>
+      </div>
+    </section>
+  );
 }
 
 export default Services;
