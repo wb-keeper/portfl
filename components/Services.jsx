cfd69fb4d4a43b14f47469262283b74f413fc498
wb-keeper <wb-keeper@mail.ru>
2024-01-02 18:30:29 +0300

    Finished Services & started Work section

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,4 +1,11 @@
 import { Blocks, GanttChartSquare, Gem } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const serviceData = [
   {
@@ -10,7 +17,6 @@ const serviceData = [
     icon: <Blocks size={72} strokeWidth={0.8} />,
     title: "Web Development",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-    lorem,
   },
   {
     icon: <Gem size={72} strokeWidth={0.8} />,
@@ -22,7 +28,31 @@ function Services(props) {
   return (
     <section className="mb-12 xl:mb-36">
       <div className="container mx-auto">
-        <h2 className="section-title">My Service</h2>
+        <h2 className="section-title mb-12 xl:mb-24 text-center mx-auto">
+          My Service
+        </h2>
+        <div className="grid xl:grid-cols-3 justify-center gap-y-12 xl:gap-y-24 xl:gap-x-8">
+          {serviceData.map((item, index) => {
+            return (
+              <Card
+                className="w-full max-w-[424px] h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative"
+                key={index}
+              >
+                <CardHeader className="text-primary absolute -top-[60px]">
+                  <div className="w-[140px] bg-white h-[80px] dark:bg-background flex justify-center items-center">
+                    {item.icon}
+                  </div>
+                </CardHeader>
+                <CardContent className="text-center">
+                  <CardTitle className="mb-4">{item.title}</CardTitle>
+                  <CardDescription className="text-lg">
+                    {item.description}
+                  </CardDescription>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
