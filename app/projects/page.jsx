5e0c01d51a3861d238a2caca6b26b779e9d57867
wb-keeper <wb-keeper@mail.ru>
2024-01-04 19:34:12 +0300

    Finished Projects page & started Contact

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import ProjectCard from "@/components/ProjectCard";
 
 const projectData = [
   {
@@ -93,7 +95,49 @@ const uniqueCategories = [
 
 function Projects(props) {
   const [categories, setCategories] = useState(uniqueCategories);
-  return <section className="min-h-screen">Projects</section>;
+  const [category, setCategory] = useState("all projects");
+  const filteredProjects = projectData.filter((project) => {
+    return category === "all projects"
+      ? project
+      : project.category === category;
+  });
+  return (
+    <section className="min-h-screen pt-12">
+      <div className="container mx-auto">
+        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+          My projects
+        </h2>
+        <Tabs defaultValue={category} className="mb-24 xl:mb-48">
+          <TabsList
+            className="w-full grid h-full md:grid-cols-4 lg:max-w-[640px] mb-12 mx-auto md:border dark:border-none
+          "
+          >
+            {categories.map((category, index) => {
+              return (
+                <TabsTrigger
+                  onClick={() => setCategory(category)}
+                  value={category}
+                  key={index}
+                  className="capitalize w-[162px] md:w-auto"
+                >
+                  {category}
+                </TabsTrigger>
+              );
+            })}
+          </TabsList>
+          <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
+            {filteredProjects.map((project, index) => {
+              return (
+                <TabsContent value={category} key={index}>
+                  <ProjectCard project={project} />
+                </TabsContent>
+              );
+            })}
+          </div>
+        </Tabs>
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
