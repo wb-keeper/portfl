05b9c50771590afc9e8aab7f41e2632ab958422d
wb-keeper <wb-keeper@mail.ru>
2024-01-05 22:41:56 +0300

    Some minor changes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,12 @@
 function Contact(props) {
-  return <div>Contact page</div>;
+  return (
+    <section>
+      <div className="container mx-auto">
+        <div>text & illustration</div>
+        <div>info text & form</div>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
