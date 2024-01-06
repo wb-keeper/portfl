4d100d1238815f13bc465e1938f8d443111f2e54
wb-keeper <wb-keeper@mail.ru>
2024-01-06 22:27:26 +0300

    some minor changes added

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,7 +2,14 @@ function Contact(props) {
   return (
     <section>
       <div className="container mx-auto">
-        <div>text & illustration</div>
+        <div>
+          <div className="flex flex-col justify-center">
+            <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
+              <span className="w-[30px] h-[2px] bg-primary"></span>
+              <div>Say Hello</div>
+            </div>
+          </div>
+        </div>
         <div>info text & form</div>
       </div>
     </section>
