442a7437813b766cd110e5b821fa6d020659a0a7
wb-keeper <wb-keeper@mail.ru>
2024-01-07 22:43:09 +0300

    Almost done

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -6,9 +6,15 @@ function Contact(props) {
           <div className="flex flex-col justify-center">
             <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
               <span className="w-[30px] h-[2px] bg-primary"></span>
-              <div>Say Hello</div>
+              Say Hello
             </div>
+            <h1 className="h1 max-w-md mb-8">Let's Work Together</h1>
+            <p className="subtitle max-w-[400px]">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum
+              facere fuga ipsam, iusto libero quae?
+            </p>
           </div>
+          <div className="hidden xl:flex w-full">illustration</div>
         </div>
         <div>info text & form</div>
       </div>
