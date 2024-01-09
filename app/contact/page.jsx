2448056b5ef598d35b263b7f819dffbd2cb194e8
wb-keeper <wb-keeper@mail.ru>
2024-01-09 23:01:15 +0300

    small changes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,10 @@
+import { MailIcon } from "lucide-react";
+
 function Contact(props) {
   return (
     <section>
       <div className="container mx-auto">
-        <div>
+        <div className="grid xl:grid-cols-2 pt-12 xl:h-[480px] mb-6 xl:mb-24">
           <div className="flex flex-col justify-center">
             <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
               <span className="w-[30px] h-[2px] bg-primary"></span>
@@ -14,9 +16,16 @@ function Contact(props) {
               facere fuga ipsam, iusto libero quae?
             </p>
           </div>
-          <div className="hidden xl:flex w-full">illustration</div>
+          <div className="hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat"></div>
         </div>
-        <div>info text & form</div>
+        <div>
+          <div>
+            <div className="flex items-center gap-x-8">
+              <MailIcon size={18} className="text-primary" />
+              <div>yourmail@gmail.com</div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
