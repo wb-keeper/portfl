814dd16c6b5b5c694fee9c53c46e536bcaf982c6
wb-keeper <wb-keeper@mail.ru>
2024-01-13 22:15:58 +0300

    Finished project

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,15 +1,29 @@
 "use client";
 
-import { User } from "lucide-react";
+import { ArrowRightIcon, MailIcon, MessageSquare, User } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 
 function Form(props) {
   return (
-    <form>
-      <div>
-        <Input />
+    <form className="flex flex-col gap-y-4">
+      <div className="relative flex items-center">
+        <Input type="name" placeholder="Name" />
         <User className="absolute right-6" size={20} />
       </div>
+      <div className="relative flex items-center">
+        <Input type="email" placeholder="Email" />
+        <MailIcon className="absolute right-6" size={20} />
+      </div>
+      <div className="relative flex items-center">
+        <Textarea placeholder="Type Your Message" />
+        <MessageSquare className="absolute top-4 right-6" size={20} />
+      </div>
+      <Button className="flex items-center max-w-[166px]">
+        Let's Talk
+        <ArrowRightIcon size={20} />{" "}
+      </Button>
     </form>
   );
 }
